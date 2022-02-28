a33cdc6213994097251c3a395d2fd1a3c580b2e0
paritat221 <95929068+paritat221@users.noreply.github.com>
2022-02-28 18:58:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.main = function(argv){
     s=[];
     for(i=0;i<g.length;i++){
         if(!('+-*/'.includes(g[i]))&&isNaN(Number(g[i])))
-            throw `Invalid input at ${e}`;
+            throw `Invalid input at ${g[i]}`;
         if(!isNaN(Number(g[i])))
             s.push(Number(g[i]));
         if('+-*/'.includes(g[i])){
