738b8c7818dfc88b22590865b9d5a40b4b238795
paritat221 <95929068+paritat221@users.noreply.github.com>
2022-03-16 19:06:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-exports.main = function(argv){
-    g=argv.slice(2);
-    s=[];
-    for(i=0;i<g.length;i++){
-        if(!isNaN(Number(g[i])))
-            s.push(Number(g[i]));
-        if('+-*/'.includes(g[i])){
-            r = eval(s[s.length-2]+g[i]+s[s.length-1]);
-            s.pop();
-            s[s.length-1]=r;
-        }
+g=process.argv.slice(2);
+s=[];
+for(i=0;i<g.length;i++){
+    if(!isNaN(Number(g[i])))
+        s.push(Number(g[i]));
+    if('+-*/'.includes(g[i])){
+        r = eval(s[s.length-2]+g[i]+s[s.length-1]);
+        s.pop();
+        s[s.length-1]=r;
     }
-    console.log(s[0]);    
 }
+console.log(s[0]);    
+
